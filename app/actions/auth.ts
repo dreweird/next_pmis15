ded05f4b3dbd2f94337fc36018f7491c6831f6ce
dreweird <andrew.auguis@gmail.com>
2026-01-07 14:03:48 +0800

    chore: Tailwind 4 migration changes  add @tailwindcss/postcss, update PostCSS config; remove client revalidatePath; remove deprecated eslint config

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -1,16 +1,12 @@
 import { signIn, signOut } from "../auth";
 import { AuthError } from "next-auth";
-import { revalidatePath } from "next/cache";
-
 
 export const login = async (provider: string) => {
   await signIn(provider, { redirectTo: "/" });
-  revalidatePath("/");
 };
 
 export const logout = async () => {
   await signOut({ redirectTo: "/" });
-  revalidatePath("/");
 };
 
 export const loginWithCreds = async (prevState: any, formData: FormData)  => {
